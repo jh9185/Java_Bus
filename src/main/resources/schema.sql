3dbb5997551c109787f71a871c5b52d5603358e4
jh9185 <89440013+jh9185@users.noreply.github.com>
2021-12-21 18:12:18 +0900

    211221_Board db file path update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,14 +6,3 @@ content varchar (30) not null ,
 name varchar (30) not null ,
 primary key(boardId)
 );
-
-INSERT INTO tbl_board( title, content, name) VALUES ('Title1',  'content1',  'name1');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title2',  'content2',  'name2');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title3',  'content3',  'name3');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title4',  'content4',  'name4');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title5',  'content5',  'name5');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title6',  'content6',  'name6');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title7',  'content7',  'name7');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title8',  'content8',  'name8');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title9',  'content9',  'name9');
-INSERT INTO tbl_board( title, content, name) VALUES ('Title10', 'content10', 'name10');
