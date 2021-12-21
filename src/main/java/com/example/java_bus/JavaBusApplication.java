cbaf17e28366dea2524b28dddaa67f159bed90fd
jh9185 <89440013+jh9185@users.noreply.github.com>
2021-12-21 13:33:48 +0900

    211221_Board Setting

---
+++ src/main/java/com/example/java_bus/JavaBusApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBusApplication.class, args);
+    }
+
+}
